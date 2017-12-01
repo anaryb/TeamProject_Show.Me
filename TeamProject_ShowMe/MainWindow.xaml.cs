56d6b970fd711f8dfcda0302832edbb004c24c87
GubskikhT <tomik1710@yandex.ru>
2017-12-01 14:43:36 +0300

    tryconnection

--- TeamProject_ShowMe/MainWindow.xaml.cs
+++ TeamProject_ShowMe/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace TeamProject_ShowMe
     {
         public MainWindow()
         {
-            InitializeComponent();//
+            InitializeComponent();//try
             int p = 0;
             //
             //hello
